70d055cbbfda76350b26abe79d7c28aee4837eda
Jake L <jakelovitz@gmail.com>
2019-04-04 16:23:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,12 +8,4 @@ def reverse_each_word (string2)
   return reverse_list2_string
 end
 
-def reverse_each_word (string)
-  list = string.split
-  reverse_list = []
-  list.each do |letter|
-    reverse_list.push(letter.reverse)
-  end
-  string_list = reverse_list.join(" ")
-  return string_list
-end
+
