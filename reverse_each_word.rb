ffac5418979364bb7ec256502996f623b058708d
Jake L <jakelovitz@gmail.com>
2019-04-04 15:57:04 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word (string)
+  list = string.split
+  reverse_list = []
+  list.each do |letter|
+    reverse_list.push(letter.reverse)
+  end
+  string_list = reverse_list.join(" ")
+  return string_list
+end
+
+def reverse_each_word (string2)
+  list2 = string2.split
+  reverse_list2 = []
+  list2.collect do |letter|
+    reverse_list2.push(letter.reverse)
+  end
+end
