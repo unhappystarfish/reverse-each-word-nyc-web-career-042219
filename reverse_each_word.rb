a9119dad8abbffa8fe14600e9b9b4d28e4b03164
Jake L <jakelovitz@gmail.com>
2019-04-04 16:16:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word (string2)
   list2.collect do |letter|
     reverse_list2.push(letter.reverse)
   end
-  reverse_list2.join
+  return reverse_list2.join
 end
 
 def reverse_each_word (string)
